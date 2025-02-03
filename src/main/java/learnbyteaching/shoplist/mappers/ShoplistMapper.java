6a44238f065ea697eb8f2bda5267f81b16a648db
서민정 <ybsm0629@gmail.com>
2025-02-03 17:50:52 +0900

    commit

--- src/main/java/learnbyteaching/shoplist/mappers/ShoplistMapper.java
+++ src/main/java/learnbyteaching/shoplist/mappers/ShoplistMapper.java
@@ -6,15 +6,20 @@ import learnbyteaching.shoplist.repository.vo.ShopItem;
 
 //	shoplist.xml과 1:1 매핑된 인터페이스
 public interface ShoplistMapper {
+	
 //	<select id="selectAllItems" resultType="shopItem">
 	List<ShopItem> selectAllItems();
+	
 //	<insert id="insertItem" parameterType="shopItem">
 	int insertItem(ShopItem item);
+	
 //	<select id="selectById" parameterType="int" 
 //			resultType="shopItem">
 	ShopItem selectById(Integer id);
+	
 //	<update id="updateItem" parameterType="shopItem">
 	int updateItem(ShopItem item);
+	
 //	<delete id="deleteItem"
 //			parameterType="int">
 	int deleteItem(Integer id);
